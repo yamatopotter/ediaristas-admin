9485fa90febf269360f5c16def6978ab521ad33f
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-12-31 16:10:41 -0300

    Primeiro commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Home</h1>
+@stop
+
+@section('content')
+    <p>Bem vindo ao E-Diaristas.</p>
+@stop
