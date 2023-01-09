7e98455ff6e341999ca7d32345ccd9ad10b245bc
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2023-01-08 22:00:43 -0300

    add: Mensagens de feedback para usuario e ajuste de validação

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ServicoController extends Controller
 {
+    /**
+     * Lista os servicos
+     * 
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function index()
     {
         $servicos = Servico::paginate(10);
@@ -16,11 +21,22 @@ class ServicoController extends Controller
         return view('servicos.index')->with('servicos', $servicos);
     }
 
+    /**
+     * Mostra o form vazio para criação
+     *
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function create()
     {
         return view('servicos.create');
     }
 
+    /**
+     * Cria um novo registro no bano de dados
+     *
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function store(ServicoRequest $request)
     {
         // $dados = $request->all();
@@ -28,16 +44,31 @@ class ServicoController extends Controller
         
         Servico::create($dados);
 
-        return redirect()->route('servicos.index');
+        return redirect()->route('servicos.index')
+        ->with('mensagem', 'Serviço criado com sucesso!');
     }
 
+    /**
+     * Mostra o formulário preenchido para atualização
+     *
+     * @param integer $id
+     * @return \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory
+     */
     public function edit(int $id)
     {
         $servico = Servico::findOrFail($id);
 
-        return view('servicos.edit')->with('servico', $servico);
+        return view('servicos.edit')->with('servico', $servico)
+        ->with('mensagem', 'Serviço atualizado com sucesso!');
     }
 
+    /**
+     * Atualiza um registro no banco de dados
+     *
+     * @param integer $id
+     * @param ServicoRequest $request
+     * @return \Illuminate\Routing\Redirector|\Illuminate\Http\RedirectResponse
+     */
     public function update(int $id, ServicoRequest $request){
         $dados = $request->except(['_token','_method']);
         $servico = Servico::findOrFail($id);
--- resources/views/servicos/index.blade.php
+++ resources/views/servicos/index.blade.php
@@ -8,6 +8,13 @@
 @stop
 
 @section('content')
+
+    @if(session('mensagem'))
+        <div class="alert alert-success">
+            {{session('mensagem')}}
+        </div>
+    @endif
+
     <table class="table">
         <thead>
             <tr>
